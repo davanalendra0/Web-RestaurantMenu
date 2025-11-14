c2fdd37ab2e392bbb49bc9ade715dc44959c6163
Indayani <davanalendra2@gmail.com>
2025-11-14 13:24:03 +0700

    update

---
+++ script.js
@@ -0,0 +1,4 @@
+window.addEventListener("load", () => {
+    document.querySelector(".hero-content").style.opacity = "1";
+    document.querySelector(".hero-content").style.transform = "translateY(0)";
+});
